94c66dbdbd5591f8d95847948d01b677a2d03753
gitit012 <147650300+gitit012@users.noreply.github.com>
2025-03-27 12:01:48 +0530

    added search bar functionality

--- FE/src/app/components/header/header.component.ts
+++ FE/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { Category } from '../../types/category';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -15,5 +16,14 @@ export class HeaderComponent {
     this.categoryService.getCategories().subscribe(result=>{
       this.categoryList = result
     })
+  }
+  router = inject(Router)
+  onSearch(e:any){
+    if(e.target.value){
+      this.router.navigateByUrl('/products?search'+e.target.value)
+    }
+  }
+  searchCategory(id:string){
+    this.router.navigateByUrl('/products?categoryId='+id!)
   }
 }
